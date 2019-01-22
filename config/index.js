43161444ec06c06e89030030e5a55584e3f9b424
hh0983 <1410315973@qq.com>
2019-01-22 13:28:36 +0800

    添加store

--- config/index.js
+++ config/index.js
@@ -11,11 +11,12 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/api':{//匹配所有以'/api'开头的请求路径
-        target:'http://localhost:4000',//代理目标的基础路径
-        changeOrigin:true,//支持跨域
-        pathRewrite:{//重写路径：去掉路径中开头的'/api'
-            '^/api':''
+      '/api':{     //匹配所有以'/api'开头的请求路径
+        //target: 'http://localhost:4000',   //代理目标的基础路径
+        target: 'http://127.0.0.1',
+        changeOrigin: true,     //支持跨域
+        pathRewrite:{    //重写路径：去掉路径中开头的'/api'
+            '^/api': ''
         }
       }
     },
